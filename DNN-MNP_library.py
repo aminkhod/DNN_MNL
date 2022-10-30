56881b6130bf3d373dafa8c5055526b785eaca09
Mohammad Amin Khodamoradi <38489600+aminkhod@users.noreply.github.com>
2022-10-30 11:38:05 +0000

    October 30th

--- DNN-MNP_library.py
+++ DNN-MNP_library.py
@@ -10,6 +10,7 @@ from builtins import isinstance
 """library_____________________________________________________________________"""
 import timeit
 from random import seed
+
 seed(1)
 start = timeit.default_timer()
 
@@ -19,14 +20,16 @@ from keras.models import Model
 from tensorflow.keras.models import Sequential
 
 import pandas as pd
-import numpy as np 
+import numpy as np
 import tensorflow.compat.v1.keras.backend as K
 import tensorflow as tf
+
 tf.compat.v1.disable_eager_execution()
 from matplotlib import pyplot as plt
 
 from Beta import Beta
 from Formula import Formula
+
 # import pandas as pd
 # from keras.constraints import Constraint
 
@@ -64,10 +67,12 @@ from keras import activations
 from keras.layers import Layer
 import tensorflow as tf
 
-                                   
 
 class DNN_MNP():
-    def __init__(this, batch = 100, iternum=200,epochs=200, activation=None):
+    def __init__(this, batch=100, iternum=200, epochs=200, activation=None):
+        this.dataset = None
+        this.target = None
+        this.new_model = None
         this.parameters = None
         this.batch = batch
         this.iternum = iternum
@@ -111,20 +116,21 @@ class DNN_MNP():
     #         Beta.BetaList.pop(self.name)
     #         del self
 
-
     class Dense1(Layer):
 
-        def __init__(self,iternum, formula, BetaList,
+        def __init__(self, iternum, formula, BetaList,
                      activation=None, **kwargs):
             self.wList = None
+            self.vList = None
+            self.errorList = None
+            self.outList = None
             self.formula = formula
             self.BetaList = BetaList
             self.activation = activations.get(activation)
             self.iternum = iternum
-    
-    
+
             super().__init__(**kwargs)
-    
+
         # def addWeight(self, name='',shape=(1,), constraint=None):
         #     return self.add_weight(name=name,shape=shape, constraint=constraint)
         def build(self, input_shape):
@@ -150,108 +156,133 @@ class DNN_MNP():
                     os = np.ones(w.shape)
                     os[self.slice] = 0
                     return w * os + zs
+
             wList = []
             for beta in self.BetaList:
-                wList.append(self.add_weight(name=beta.name,shape=(1,),
-                constraint=FreezeSlice([beta.initial_value],np.s_[[0]]) if beta.constraint == 1 else None))
+                wList.append(self.add_weight(name=beta.name, shape=(1,),
+                                             constraint=FreezeSlice([beta.initial_value],
+                                                                    np.s_[[0]]) if beta.constraint == 1 else None))
                 # self.W1 = self.add_weight(name='Ba',shape=(1,), constraint=FreezeSlice([3.0],np.s_[[0]]))
                 # self.W2 = self.add_weight(name='Bb',shape=(1,))
                 # self.W3 = self.add_weight(name='Bp',shape=(1,))
                 # self.W4 = self.add_weight(name='Bq',shape=(1,))
                 # self.W5 = self.add_weight(name='cor',shape=(1,),initializer = tf.keras.initializers.Constant(0.5),
                 #                           trainable=True,constraint=tf.keras.constraints.MinMaxNorm(max_value=0.98))
-            print(wList)
+            # print(wList)
             self.wList = wList
             super().build(input_shape)
-    
+
         def call(self, inputs, **kwargs):
-            ErrorList = []
+            errorList = []
             vList = []
+            outList = []
+            r, c = inputs.shape
+            formulaindex = 0
             for formula in self.formula:
-                ErrorList.append(np.random.normal(loc = 0, scale = 1 , size = (self.iternum,)))
-            # Error1= np.random.normal(loc = 0, scale = 1 , size = (self.iternum,))
-            # Error2= np.random.normal(loc = 0, scale = 1 , size = (self.iternum,))
+                error = np.random.normal(loc=0, scale=1, size=(self.iternum,))
+                errorList.append(error)
+                # print(type(formula.args))
 
+                # Error1= np.random.normal(loc = 0, scale = 1 , size = (self.iternum,))
+                # Error2= np.random.normal(loc = 0, scale = 1 , size = (self.iternum,))
 
-    
-            # inp=tf.cast(tf.transpose(tf.stack((Error1,Error2))), tf.float32)
-            # o=tf.constant([0.0])
-            # p=tf.constant([1.0])
-            #
-            # row1=tf.reshape(tf.stack([p,o]),(2,))
-            #
-            # row2=tf.reshape(tf.stack([self.W5,tf.sqrt(p-tf.square(self.W5))]),(2,))
-            #
-            # L = tf.stack([row1,row2])
-            #
-            # error=self.activation(tf.matmul(inp,L))
-    
-            v1 = tf.expand_dims((self.W1 * inputs[:,0]) + (self.W2 * inputs[:,1]) + (self.W3 * inputs[:,2]) + (self.W4 * inputs[:,3]),axis = 1)
-            
-    
-            out1=tf.expand_dims(tf.matmul(v1,tf.ones((1, self.iternum), tf.float32))+ error[:,0],axis = 1)
-            
-    
-    
-            v2 =tf.expand_dims((self.W1 * inputs[:,4]) + (self.W2 * inputs[:,5]) + (self.W3 * inputs[:,6])+ (self.W4 * inputs[:,7]),axis = 1)
-    
-            out2=tf.expand_dims(tf.matmul(v2,tf.ones((1, self.iternum), tf.float32))+ error[:,1],axis = 1)
-            out1 = out1 - out2
-            
-            out2 = out2 - out2
-            
-            Scores = tf.experimental.numpy.hstack((out1, out2))
-            
+                # inp=tf.cast(tf.transpose(tf.stack((Error1,Error2))), tf.float32)
+                # o=tf.constant([0.0])
+                # p=tf.constant([1.0])
+                #
+                # row1=tf.reshape(tf.stack([p,o]),(2,))
+                #
+                # row2=tf.reshape(tf.stack([self.W5,tf.sqrt(p-tf.square(self.W5))]),(2,))
+                #
+                # L = tf.stack([row1,row2])
+                #
+                # error=self.activation(tf.matmul(inp,L))
+
+                # v1 = (self.W1 * inputs[:, 0]) + (self.W2 * inputs[:, 1]) + \
+                #      (self.W3 * inputs[:, 2]) + (self.W4 * inputs[:, 3])
+                # out1 = tf.expand_dims(tf.matmul(v1, tf.ones((1, self.iternum), tf.float32)) + error[:, 0], axis=1)
+                #
+                # v2 = tf.expand_dims((self.W1 * inputs[:, 4]) + (self.W2 * inputs[:, 5]) +
+                #                     (self.W3 * inputs[:, 6]) + (self.W4 * inputs[:, 7]), axis=1)
+                #
+                # out2 = tf.expand_dims(tf.matmul(v2, tf.ones((1, self.iternum), tf.float32)) + error[:, 1], axis=1)
+                # out1 = out1 - out2
+                # out2 = out2 - out2
+
+                weight_input = 0
+                betaindex, inputindex = 0, 0  # index for Betas and inputs
+                for arg in formula.args:
+                    if isinstance(arg, tuple):
+                        weight_input += (self.wList[betaindex] * inputs[:, inputindex])
+                        inputindex += 1
+                        betaindex += 1
+                    elif isinstance(arg, Beta):
+                        weight_input += (self.wList[betaindex])
+                        betaindex += 1
+
+                    v = tf.expand_dims(weight_input, axis=1)
+                    out = tf.expand_dims(tf.matmul(v, tf.ones((1, self.iternum),
+                                                              tf.float32)) + error, axis=1)
+                    vList.append(v)
+                    outList.append(out)
+
+                formulaindex += 1
+
+            self.errorList = errorList
+            self.outList = outList
+            self.vList = vList
+            print('errorList', self.errorList)
+            print('self.outList', self.outList)
+            print('self.vList', self.vList)
+
+            Scores = tf.experimental.numpy.hstack(tuple(outList))
+
             return Scores
 
-
-        def F(x):  
+        def F(x):
             return x
 
-
     def creat_model(this, formula, BetaList):
-        #Dense1 = this.Dense1(iternum=this.iternum, activation=this.activation)
+        # Dense1 = this.Dense1(iternum=this.iternum, activation=this.activation)
         Utility1 = Sequential()
 
-
-
-        Utility1.add(tf.keras.layers.InputLayer((8,),name='inp_1'))
+        Utility1.add(tf.keras.layers.InputLayer((8,), name='inp_1'))
         Utility1.add(this.Dense1(formula=formula, BetaList=BetaList, iternum=this.iternum))
 
-        mergedOutput1=Utility1.output
+        mergedOutput1 = Utility1.output
 
-        mergedOutput=tf.transpose(mergedOutput1, perm=[0,2,1])
+        mergedOutput = tf.transpose(mergedOutput1, perm=[0, 2, 1])
 
-        beta=1e4
-        mergedOutput = tf.nn.softmax(mergedOutput*beta)
+        beta = 1e4
+        mergedOutput = tf.nn.softmax(mergedOutput * beta)
 
-        mergedOutput=tf.reduce_sum(mergedOutput, 1)
+        mergedOutput = tf.reduce_sum(mergedOutput, 1)
 
-        Sum=tf.reduce_sum(mergedOutput, 1)
+        Sum = tf.reduce_sum(mergedOutput, 1)
 
-        main_network= tf.transpose(tf.divide(tf.transpose(mergedOutput),Sum))
+        main_network = tf.transpose(tf.divide(tf.transpose(mergedOutput), Sum))
 
         new_model = Model(
-        inputs=[Utility1.input], outputs=[main_network])
-
+            inputs=[Utility1.input], outputs=[main_network])
 
         print(new_model.summary())
         this.new_model = new_model
         return new_model
 
-
-    def fit_model(this, dataset, target):
-        this.dataset = dataset
+    def fit_model(this, target):
+        this.dataset = Formula.dataFrame
         this.target = target
-        this.new_model.compile(optimizer= 'adam', loss='categorical_crossentropy'  ,metrics=['categorical_accuracy'])
+        this.new_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
         weights_dict = {}
 
-        cbk = tf.keras.callbacks.LambdaCallback( on_epoch_end=lambda epoch, logs: weights_dict.update({epoch:this.new_model.get_weights()}))
+        cbk = tf.keras.callbacks.LambdaCallback(
+            on_epoch_end=lambda epoch, logs: weights_dict.update({epoch: this.new_model.get_weights()}))
 
-        callback = tf.keras.callbacks.EarlyStopping( monitor='loss',patience=20,mode='min')
+        callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=20, mode='min')
 
-        history= this.new_model.fit( this.dataset, target, epochs=this.epochs ,batch_size=this.batch,shuffle=True,callbacks=[cbk])
+        history = this.new_model.fit(this.dataset, target, epochs=this.epochs, batch_size=this.batch, shuffle=True,
+                                     callbacks=[cbk])
         this.weights_dict = weights_dict
         this.history = history
         return this.new_model
@@ -261,33 +292,30 @@ class DNN_MNP():
     def Estimated_parameters(this):
         weight = [layer.get_weights() for layer in this.new_model.layers]
 
-        a = [weight[1][0],weight[1][1],weight[1][2],weight[1][3],weight[1][4]]
+        a = [weight[1][0], weight[1][1], weight[1][2], weight[1][3], weight[1][4]]
 
         this.parameters = np.array(a)
         return this.parameters
 
     """Extracting Weights__________________________________________________________"""
+
     def plot_parameters_history(this):
-        weights_epochs = np.zeros((5,this.epochs))
+        weights_epochs = np.zeros((5, this.epochs))
 
         for i in range(this.epochs):
-            weights_epochs[0,i]=this.weights_dict[i][0]
-            weights_epochs[1,i]=this.weights_dict[i][1]
-            weights_epochs[2,i]=this.weights_dict[i][2]
-            weights_epochs[3,i]=this.weights_dict[i][3]
-            weights_epochs[4,i]=this.weights_dict[i][4]
+            weights_epochs[0, i] = this.weights_dict[i][0]
+            weights_epochs[1, i] = this.weights_dict[i][1]
+            weights_epochs[2, i] = this.weights_dict[i][2]
+            weights_epochs[3, i] = this.weights_dict[i][3]
+            weights_epochs[4, i] = this.weights_dict[i][4]
 
-#ploting parameters history
-            epoch = np.arange(1, this.epochs+1, 1)
-            plt.plot(epoch, weights_epochs[0, :], label = "Ba")
-            plt.plot(epoch, weights_epochs[1, :],label = "Bb")
-            plt.plot(epoch, weights_epochs[2, :],label = "Bp")
-            plt.plot(epoch, weights_epochs[3, :],label = "Bq")
-            plt.plot(epoch, weights_epochs[4, :],label = "correlation")
-
-
-
-
+            # ploting parameters history
+            epoch = np.arange(1, this.epochs + 1, 1)
+            plt.plot(epoch, weights_epochs[0, :], label="Ba")
+            plt.plot(epoch, weights_epochs[1, :], label="Bb")
+            plt.plot(epoch, weights_epochs[2, :], label="Bp")
+            plt.plot(epoch, weights_epochs[3, :], label="Bq")
+            plt.plot(epoch, weights_epochs[4, :], label="correlation")
 
     # plt.legend(loc="upper right")
     # plt.title('weights')
@@ -317,19 +345,17 @@ class DNN_MNP():
     # plt.legend(loc='best')
     # plt.show()
 
-
     """ Time """
     # stop = timeit.default_timer()
 
     # print('Time: ', stop - start
 
-
-
     """ list all data in history___________________________________________________"""
-    
+
     # print(history.history.keys())
-    
+
     """ summarise history for accuracy_____________________________________________"""
+
     def summarise_history_accuracy(this):
         plt.plot(this.history.history['categorical_accuracy'])
         plt.title('model accuracy')
@@ -340,6 +366,7 @@ class DNN_MNP():
         plt.show()
 
     """ summarise history for loss_________________________________________________"""
+
     def summarise_history_loss(this):
         plt.plot(this.history.history['loss'])
         plt.title('model loss')
@@ -375,7 +402,6 @@ class DNN_MNP():
 
             return Hessian
 
-
         mat = get_inverse_Hessian(this.new_model, this.dataset, this.target)
         invHess = np.linalg.inv(mat)
 
@@ -383,7 +409,6 @@ class DNN_MNP():
 
         stds = [invHess_abs[i][i] ** 0.5 for i in range(invHess_abs.shape[0])]
         return stds
-
 
     """STANDARD ERROR______________________________________________________________"""
 
@@ -408,85 +433,82 @@ class DNN_MNP():
     #
     # stds1 = [invHess1[i][i]**0.5 for i in range(invHess1.shape[0])]
 
-
-
     """End_________________________________________________________________________"""
-
 
     # stop = timeit.default_timer()
 
     # print('Time: ', stop - start)
+
+
 if __name__ == '__main__':
-        ddd = DNN_MNP()
-        Dataset = pd.read_excel('Dataset_MNP.xlsx')
+    ddd = DNN_MNP()
+    Dataset = pd.read_excel('Dataset_MNP.xlsx')
 
-        Dataset.drop('Unnamed: 0', inplace=True, axis=1)
-        target = ddd.dense_to_one_hot(Dataset['choice'], 2)
-        ddd.attach(Dataset)
-        # print(Dataset.columns)
+    Dataset.drop('Unnamed: 0', inplace=True, axis=1)
+    target = ddd.dense_to_one_hot(Dataset['choice'], 2)
+    ddd.attach(Dataset)
+    # print(Dataset.columns)
 
-        ASC_TRAIN = Beta('ASC', 0, 0)
-        ASC_SM = Beta('ASCc', 0, 0)
-        ASC = Beta('newASC', 4, 1)
-        # print(type(ASC_SM))
-        f1 = Formula((ASC_TRAIN, a1), (ASC_SM, b1))
-        f2 = Formula((ASC_TRAIN, b1))
-        f3 = Formula((ASC))
-        # print(Formula.formulaList[0].get_args())
-        # print(Formula.formulaList)
-        for f in Formula.formulaList:
-            print(f.get_args())
-        # f2 = formula()
+    ASC_TRAIN = Beta('ASC', 0, 0)
+    ASC_SM = Beta('ASCc', 0, 0)
+    ASC = Beta('newASC', 4, 1)
+    # print(type(ASC_SM))
+    f1 = Formula((ASC_TRAIN, a1), (ASC_SM, b1))
+    f2 = Formula((ASC_TRAIN, b1))
+    f3 = Formula((ASC))
+    # print(Formula.formulaList[0].get_args())
+    # print(Formula.formulaList)
+    # for f in Formula.formulaList:
+    #     print(f.get_args())
+    # print(Formula.dataFrame)
 
-        ddd.creat_model(formula=Formula.formulaList, BetaList=Beta.BetaList)
-        # ddd.fit_model(Dataset.iloc[:,:-1], target)
+    ddd.creat_model(formula=Formula.formulaList, BetaList=Beta.BetaList)
+    # ddd.fit_model(target)
 
+    # newModel = DNN_MNP()
 
-        # newModel = DNN_MNP()
+    # Bp = -1
+    # Ba = 0.5
+    # Bb = 0.5
+    # Bq = 1
 
+    # Dataset = pd.read_excel('Dataset_MNP.xlsx')
 
-        # Bp = -1
-        # Ba = 0.5
-        # Bb = 0.5
-        # Bq = 1
+    # cor = pd.read_excel('Cor_MNP.xlsx')
 
-        # Dataset = pd.read_excel('Dataset_MNP.xlsx')
+    # cor.drop('Unnamed: 0', inplace=True, axis=1)
 
-        # cor = pd.read_excel('Cor_MNP.xlsx')
+    # True_val= [Bp,Ba,Bb,Bq,cor.iloc[1,0]]
 
-        # cor.drop('Unnamed: 0', inplace=True, axis=1)
+    # Dataset.drop('Unnamed: 0', inplace=True, axis=1)
 
-        # True_val= [Bp,Ba,Bb,Bq,cor.iloc[1,0]]
+    # def dense_to_one_hot(labels_dense, num_classes):
+    #     labels_dense = labels_dense.astype(np.int64)
+    #     num_labels = labels_dense.shape[0]
+    #     index_offset = np.arange(num_labels) * num_classes
+    #     labels_one_hot = np.zeros((num_labels, num_classes))
+    #     labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
 
-        # Dataset.drop('Unnamed: 0', inplace=True, axis=1)
+    #     return labels_one_hot
 
-        # def dense_to_one_hot(labels_dense, num_classes):
-        #     labels_dense = labels_dense.astype(np.int64)
-        #     num_labels = labels_dense.shape[0]
-        #     index_offset = np.arange(num_labels) * num_classes
-        #     labels_one_hot = np.zeros((num_labels, num_classes))
-        #     labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
-
-        #     return labels_one_hot
-
-        # classes = dense_to_one_hot(labels_dense=Dataset['choice'], num_classes=2)
-        # matrix1 = np.zeros((4,4))
-        #
-        # Data = Dataset.iloc[:,0:8]
-        #
-        # y_pred = new_model.predict(Dataset.iloc[:, 1:11 ])
-        #
-        #
-        # pred = pd.DataFrame(np.concatenate((y_pred[:,0:1],y_pred[:,0:1],
-        #                                     y_pred[:,1:2],y_pred[:,1:2]), axis= 1))
-        #
-        # for i in range(4):
-        #     for j in range(4):
-        #         matrix1[i,j]= np.sum((Data.iloc[:,i] * Data.iloc[:,j]) * (pred.iloc[:,i]*pred.iloc[:,j]))
-        #
-        #
-        #
-        #
-        # invHess1 = np.linalg.inv(matrix1)
-        #
-        # stds1 = [invHess1[i][i]**0.5 for i in range(invHess1.shape[0])]
+    # classes = dense_to_one_hot(labels_dense=Dataset['choice'], num_classes=2)
+    # matrix1 = np.zeros((4,4))
+    #
+    # Data = Dataset.iloc[:,0:8]
+    #
+    # y_pred = new_model.predict(Dataset.iloc[:, 1:11 ])
+    #
+    #
+    # pred = pd.DataFrame(np.concatenate((y_pred[:,0:1],y_pred[:,0:1],
+    #                                     y_pred[:,1:2],y_pred[:,1:2]), axis= 1))
+    #
+    # for i in range(4):
+    #     for j in range(4):
+    #         matrix1[i,j]= np.sum((Data.iloc[:,i] * Data.iloc[:,j]) * (pred.iloc[:,i]*pred.iloc[:,j]))
+    #
+    #
+    #
+    #
+    # invHess1 = np.linalg.inv(matrix1)
+    #
+    # stds1 = [invHess1[i][i]**0.5 for i in range(invHess1.shape[0])]
--- Formula.py
+++ Formula.py
@@ -1,13 +1,24 @@
 from Beta import Beta
+import pandas as pd
+
 class Formula:
     formulaList = []
-
+    dataFrame = pd.DataFrame()
     def __init__(self, *args):
 
         # self.error = False
         self.args = args
         # if name not in Formula.FormulaList:
         Formula.formulaList.append(self)
+
+        z = list(Formula.dataFrame.columns)
+        for arg in self.args:
+            # print(type(arg))
+            if not isinstance(arg, Beta):
+                z.append(arg[1].name)
+                Formula.dataFrame = pd.concat([Formula.dataFrame, arg[1]], axis=1)
+                Formula.dataFrame.columns = z
+
         # else:
         #     self.error = True
         #     raise Exception('The Formula name is taken before. Please s elect unique name for your Formula.')
